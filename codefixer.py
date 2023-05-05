59f70d4e5aabf466b8f5a6a3b30b9240b516863d
Mayank Kumar <mayank@draup.com>
2023-05-05 13:09:39 +0530

    precompute feature

--- codefixer.py
+++ codefixer.py
@@ -8,48 +8,6 @@ import difflib
 import os
 
 
-# Your provided functions go here
-def get_updated_code_old(prompt):
-    """
-    Get the updated code from GPT-3 API
-    Args: prompt: str
-    Returns: str
-    """
-    # sending request to prompt the GPT-3 API
-    print("----- sending request to prompt the GPT-3 API -----")
-    start_time = time.time()
-
-    # Set up the OpenAI API credentials openai.
-    openai.api_key = os.environ.get("OPENAI_API_KEY")
-    model_engine = "text-davinci-003"
-    # Set the GPT-3 model engine to use
-    # Generate corrected code using GPT-3.5 API
-    response = openai.Completion.create(engine=model_engine,
-                                        prompt=prompt,
-                                        max_tokens=1024,
-                                        n=1,
-                                        stop="\n\n",
-                                        temperature=0.2, )
-    end_time = time.time()
-    # print the time taken to get the response in seconds
-    print("----- Time taken to get the response in seconds: ", end_time - start_time, "-----")
-    print("----- Got response from GPT-3 API -----")
-    # Extract the corrected code from the response
-    corrected_code = response.choices[0].text.strip()
-    # split by \n
-    corrected_code = corrected_code.split('\n')
-    # remove empty lines from last and first
-    while corrected_code[0] == '':
-        corrected_code.pop(0)
-    while corrected_code[-1] == '':
-        corrected_code.pop(-1)
-    # remove pass from last line
-    if 'pass' in corrected_code[-1].strip():
-        corrected_code.pop(-1)
-    # join using \n
-    corrected_code = '\n'.join(corrected_code)
-    return corrected_code
-
 def save_dict_to_csv_file(data, model_name):
     """
     Save the dictionary to a csv file
@@ -63,102 +21,10 @@ def save_dict_to_csv_file(data, model_name):
         f.write(f"{data['prompt_tokens']},{data['completion_tokens']},{model_name}\n")
 
 
-def extract_python_code(text):
-    code_pattern = re.compile(r'<code>(.*?)<\/code>', re.DOTALL)
-    code_matches = code_pattern.findall(text)
-
-    if code_matches:
-        code = ''.join(code_matches)
-        return code.replace('\n\n', '\n')
-    else:
-        return None
-
-def get_updated_code_3(prompt):
-    """
-    Get the updated code from GPT-3 API
-    Args: prompt: str
-    Returns: str
-    """
-
-    # Set up the OpenAI API credentials
-    openai.api_key = os.environ.get("OPENAI_API_KEY_3")
-    model_engine = "gpt-4"
-
-    # Generate corrected code using GPT-3.5 API
-    response = openai.ChatCompletion.create(
-        model=model_engine,
-        messages=[
-            {"role": "system", "content": "You are a automated ai python code fixer that takes partial python code and fixes it without giving explanation and  without changing starting code or ending code."},
-            {"role": "user", "content": prompt}
-        ]
-    )
-
-    save_dict_to_csv_file(response["usage"])
-    # Extract the corrected code from the response
-    corrected_code = response.choices[0].message.content.strip()
-    # extract code inside \n\n use regex
-    code_pattern = re.compile(r'```(.*?)```', re.DOTALL)
-    code_matches = code_pattern.findall(corrected_code)
-
-    # remove { } from code
-    if code_matches:
-        code = ''.join(code_matches)
-        code = code.replace('\n\n', '\n')
-        # remove 'python' from code
-        code = code.replace('python', '')
-        code = code.replace('{', '')
-        code = code.replace('}', '')
-        # trim code till 'code:' is found if not found return code
-        if 'code:' in code:
-            code = code[code.find('code:') + len('code:'):]
-        if 'Here' in code:
-            # trim from 'Here's' till the fisrt : is found
-            code = code[code.find(':') + 1:]
-
-        # if last line starts with pass remove it
-        code_in_lines = code.split('\n')
-        # pop from  back if linestarts with empty
-        for i in range(len(code_in_lines) - 1, -1, -1):
-            if code_in_lines[i] == '':
-                code_in_lines.pop(i)
-            else:
-                break
-        if code_in_lines[-1].strip().startswith('pass'):
-            code_in_lines = code_in_lines[:-1]
-        code = '\n'.join(code_in_lines)
-        return code
-    else:
-        remove_words = ('code:', 'Code:')
-        for i in remove_words:
-            if i in corrected_code:
-                corrected_code = corrected_code[corrected_code.find(i) + len(i):]
-        if 'Here' in corrected_code:
-            # trim from 'Here's' till the first : is found
-            code = corrected_code[corrected_code.find(':') + 1:]
-
-        code_in_lines = corrected_code.split('\n')
-        removed_lines = 0
-        for i in range(len(code_in_lines) - 1, -1, -1):
-            if code_in_lines[i] == '':
-                code_in_lines.pop(i)
-                removed_lines += 1
-            else:
-                break
-        if code_in_lines[-1].strip().startswith('pass'):
-            code_in_lines = code_in_lines[:-1]
-        # add empty lines at the end
-        for i in range(removed_lines):
-            code_in_lines.append('\n ')
-        corrected_code = '\n'.join(code_in_lines)
-        if 'Here' in corrected_code:
-            # trim from 'Here's' till the fisrt : is found
-            corrected_code = corrected_code[corrected_code.find(':') + 1:]
-
-        return corrected_code
 
 def get_updated_code(prompt):
     """
-    Get the updated code from GPT-3 API
+    Get the updated code from GPT-4 API
     Args: prompt: str
     Returns: str
     """
@@ -178,7 +44,7 @@ def get_updated_code(prompt):
     )
     end_time = time.time()
     response_time = end_time - start_time
-    print(response)
+    # print(response)
     save_dict_to_csv_file(response["usage"], response['model'])
     # Extract the corrected code from the response
     corrected_code = response.choices[0].message.content.strip()
@@ -194,7 +60,11 @@ def get_updated_code(prompt):
         code_matches = code_matches[0]
 
     code_pattern_2 = re.compile(r'<python>(.*?)</python>', re.DOTALL)
-    code_matches_2 = code_pattern_2.findall(code_matches)[0]
+    code_matches_2 = code_pattern_2.findall(code_matches)
+    if not code_matches_2:
+        code_matches_2 = code_matches
+    else:
+        code_matches_2 = code_matches_2[0]
     if 'code' in code_matches_2:
         code_matches_2 = code_matches_2[code_matches_2.find('code:') + len('code:'):]
     return code_matches_2, response_time
@@ -219,7 +89,7 @@ def get_sonar_report_data(filename='sonarqube_bugs.json'):
             flow = issue['flows'][0]
             location = flow['locations'][0]
             component = location['component']
-            d['file_path'] = "./" + component.split(':')[-1]
+            d['file_path'] = component.split(':')[-1]
             d['start_line'] = location['textRange']['startLine']
             d['end_line'] = location['textRange']['endLine']
             d['message'] = issue['message']
@@ -227,37 +97,6 @@ def get_sonar_report_data(filename='sonarqube_bugs.json'):
         except:
             pass
     return all_data
-
-def extract_code_context(file_path, bug_line, context_lines=5):
-    with open(file_path, 'r') as f:
-        lines = f.readlines()
-
-    start_line = max(bug_line - context_lines - 1, 0)
-    end_line = min(bug_line + context_lines, len(lines))
-
-    open_delimiters = {'(', '[', '{'}
-    close_delimiters = {')', ']', '}'}
-    delimiter_stack = []
-
-    def update_delimiter_stack(line):
-        for char in line:
-            if char in open_delimiters:
-                delimiter_stack.append(char)
-            elif char in close_delimiters:
-                if delimiter_stack and char == close_delimiters[delimiter_stack[-1]]:
-                    delimiter_stack.pop()
-
-    # Adjust the start_line to ensure we don't begin with a partial line
-    while start_line > 0:
-        current_line = lines[start_line].strip()
-        previous_line = lines[start_line - 1].strip()
-        update_delimiter_stack(previous_line)
-        if not delimiter_stack and (not current_line or current_line[0] not in close_delimiters):
-            break
-        start_line -= 1
-
-    code_context = lines[start_line:end_line]
-    return start_line + 1, ''.join(code_context)
 
 
 def get_new_block_as_string(file_path, start_line, end_line):
@@ -418,10 +257,10 @@ class CodeFixerUI:
         self.message_label = ttk.Label(self.main_frame, text="SonarQube Message:")
         self.message_label.grid(row=0, column=0, sticky=(tk.W, tk.N))
         self.message_text = tk.Text(self.main_frame, wrap=tk.WORD, height=2)
-        self.message_text.grid(row=1, column=0, columnspan=2, padx=(10, 0), sticky=(tk.W, tk.E, tk.N, tk.S))
+        self.message_text.grid(row=1, column=0, columnspan=2, padx=(0, 0), sticky=(tk.W, tk.E, tk.N, tk.S))
 
         self.response_time_text = tk.Text(self.main_frame, wrap=tk.WORD, height=1)
-        self.response_time_text.grid(row=4, column=0, columnspan=3, padx=(10, 0), sticky=(tk.W, tk.E, tk.N, tk.S))
+        self.response_time_text.grid(row=4, column=0, columnspan=3, padx=(0, 0), sticky=(tk.W, tk.E, tk.N, tk.S))
 
         # Create the buttons
         self.ignore_button = ttk.Button(self.main_frame, text="Ignore", command=self.ignore_bug)
@@ -448,9 +287,10 @@ class CodeFixerUI:
         self.current_bug_index = 0
         self.path = None
 
-        # Load the first bug on application start
-        global code_data
-        self.code_data = self.process_bug()
+        self.precompute_all_suggestions()
+        # # Load the first bug on application start
+        # global code_data
+        # self.code_data = self.process_bug()
 
     def highlight_differences(self):
         content1 = self.prev_code_text.get('1.0', tk.END).splitlines()
@@ -482,18 +322,10 @@ class CodeFixerUI:
         self.prev_code_text.tag_configure('removed', background='lightcoral')
         self.updated_code_text.tag_configure('added', background='lightgreen')
 
-    def close_diff_window(self):
-        try:
-            if self.result_window:
-                self.result_window.destroy()
-        except:
-            pass
-
     def retry(self):
         suggested_code = self.code_data[1]
         self.process_bug(suggested_code)
         return
-
 
     def ignore_bug(self):
         # Ignore the bug and move to the next bug
@@ -516,6 +348,48 @@ class CodeFixerUI:
         self.process_bug()
         return
 
+    def write_precomputed_suggestions_to_file(self):
+        # self.data is a list of dictionaries, write it to a json file
+        with open('precomputed_suggestions.json', 'w') as f:
+            json.dump(self.data, f)
+
+    def precompute_all_suggestions(self):
+        if os.environ.get('PRECOMPUTED') == 'NO':
+            for d in self.data:
+                path = os.environ.get('CODE_PATH') + d['file_path']
+                code, start_index, end_index = \
+                    get_new_block_as_string(path, d['start_line'], d['end_line'])
+                fix_msg = d['message']
+                fix_msg = update_line_number(fix_msg, d['start_line'] - start_index)
+                msg = f"""Here's a partial Python code with a bug inside <python> </python>:
+                            code: \n {code} \n 
+                            line number: {d['start_line'] - start_index}
+                            message: {fix_msg}
+                            Strictly follow the following
+                            instructions:
+                            * Use message to get the context of the bug
+                            * Fix the bug
+                            * don't include any explanation 
+                            * correct the indentation before sending the code
+                            * send code inside code block
+                            response format : 
+                            <python>
+                            print('hello world')
+                            </python>
+                            """
+                updated_code, response_time = get_updated_code(msg)
+                d['suggested_code'] = updated_code
+                d['response_time'] = response_time
+            self.write_precomputed_suggestions_to_file()
+
+        global code_data
+        self.code_data = self.process_bug()
+
+    def get_data_from_precomputed_file(self):
+        with open('precomputed_suggestions.json', 'r') as f:
+            data = json.load(f)
+        return data
+
     def process_bug(self, wrong_code=None):
         if self.current_bug_index < len(self.data):
             # add fetching message to the both windows
@@ -523,8 +397,9 @@ class CodeFixerUI:
             self.prev_code_text.insert(1.0, 'Fetching...')
             self.updated_code_text.delete(1.0, tk.END)
             self.updated_code_text.insert(1.0, 'Fetching...')
+            self.data = self.get_data_from_precomputed_file()
             d = self.data[self.current_bug_index]
-            self.path = "/Users/Mayank/Desktop/codebase/draup-server/" + d['file_path'][2:]
+            self.path = os.environ.get('CODE_PATH') + d['file_path']
             code, start_index, end_index = \
                 get_new_block_as_string(self.path, d['start_line'], d['end_line'])
             wrong_suggestion_message = None
@@ -552,9 +427,12 @@ class CodeFixerUI:
             <python>
             print('hello world')
             </python>
-            {str(wrong_suggestion_message)}
+            {str(wrong_suggestion_message) if wrong_suggestion_message else ''}
             """
-            updated_code, response_time = get_updated_code(msg) # Display the previous code in the UI
+            if wrong_code:
+                updated_code, response_time = get_updated_code(msg)  # Display the previous code in the UI
+            else:
+                updated_code, response_time = d['suggested_code'], d['response_time']
             print("-----old code-----")
             print(code)
             print("-----new code-----")
@@ -606,8 +484,10 @@ class CodeFixerUI:
             # Display a message when there are no more bugs
             self.prev_code_text.delete(1.0, tk.END)
             self.updated_code_text.delete(1.0, tk.END)
+            self.message_text.delete(1.0, tk.END)
             self.prev_code_text.insert(tk.END, "No more bugs to process")
             self.updated_code_text.insert(tk.END, "No more bugs to process")
+            self.message_text.insert(tk.END, "No more bugs to process")
 
 
 def main():
